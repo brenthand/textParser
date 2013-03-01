4d992bf6a065dc3be8b7f1f0d9f49c92a45a9b8b
brent hand <d5tz@debian.hsd1.fl.comcast.net>
2013-03-01 09:15:46 -0500

    new version added bible_parser.py

---
+++ bible_parser.py
@@ -0,0 +1,64 @@
+book_names = {}
+cur_book = ""
+cur_chapter = ""
+cur_verse = ""
+html_bible = "<!DOCTYPE>\n<html>\n<body>"
+
+#create dictionary of book names
+for line in open('book_names.txt', 'r'):
+    line = line.split(None, 1)
+    key = line[0].strip()
+    value = line[1].strip()
+    if key in book_names:
+        pass
+    else:
+        book_names[key] = [value]
+
+
+
+#parse file line by line to create html file
+with open('asv.txt', 'r') as f:
+	for line in f:
+	    line = line.split(None, 4)
+	    book = line[0]
+	    chapter = line[1]
+
+	    if chapter != cur_chapter and book != cur_book:
+		if cur_chapter == "" or cur_book == "":
+		    pass
+		else:
+		    html_bible += "</div CHAPTER>\n"
+		    html_bible += "</div BOOK>\n"
+		cur_chapter = chapter
+		cur_book = book
+		html_bible += '<div class="book" id="' + book_names[book][0] + '">\n'
+		html_bible += '<div class="chapter" id="' + chapter + '">\n'
+	    
+	    elif chapter != cur_chapter:
+		#if cur chapter is blank we dont need to close the tag
+		if cur_chapter == "":
+		    pass
+		else: 
+		    html_bible += "</div CHAPTER>\n"
+
+		cur_chapter = chapter
+		html_bible += '<div class="chapter" id="' + chapter + '">\n'
+
+	    elif book != cur_book:
+		if cur_book == "":
+		    pass
+		else:
+		    html_bible += "</div BOOK>\n"
+		cur_book = book
+		html_bible += '<div class="book" id="' + book_names[book][0] + '">\n'
+	    
+	    verse = line[2]
+	    text = line[4]
+	    html_bible += '<p class="verse" id="' + verse + '">' + text + '</p>\n' 
+
+html_bible += "</body>\n</html>"
+
+#print html_bible
+
+open("bible.html", "w").write(html_bible)
+
